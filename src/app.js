9534729942260562e7d1ae50b8919923134ae691
pramadiputra <pramadiputra@outlook.com>
2018-11-13 11:27:33 +0800

    Adding edit, and update todo list

--- src/app.js
+++ src/app.js
@@ -3,27 +3,38 @@ var app = new Vue({
     data: {
         message: 'Hello Vuex!',
         todo: null,
+        editTodo: null,
         todos: [],
     },
     methods: {
         add(todo) {
-            if(!this.isTodoValid) {
-                return;
-            }
+            if(!this.isTodoValid) { return; }
+
             this.todos.push({
                 content: todo,
                 finished: false,
+                isEdited: false,
             });
             
             this.todo = null;
+        },
+        edit(todo) {
+            this.editTodo = todo.content;
+            todo.isEdited = true;
+        },
+        update(todo) {
+            if(!this.isEditTodoValid) { return; }
+
+            todo.content = this.editTodo;
+            todo.isEdited = false;
         },
         remove(todo) {
             this.todos = this.todos.filter(item => item != todo);
         },
     },
     computed: {
-        isTodoValid() {
-            return !!this.todo; 
-        }
+        isTodoValid() { return !!this.todo; },
+        isEditTodoValid() { return !!this.editTodo; },
+        isTodoBeingEdited() { return this.todos.filter(todo => todo.isEdited).length > 0; },
     }
 });
